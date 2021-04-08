e4cd54364da03acd1a9f15cbcd4f8ea2049904fb
Guntter <ggunty96@gmail.com>
2021-04-08 17:39:37 +0300

    added proper dev url for android; enabled multidex (android)

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,6 +1,8 @@
+import { Platform } from 'react-native';
+
 const __DEV__ = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
-const API_DEV_URL = 'http://localhost:8000/api';
+const API_DEV_URL = Platform.OS === 'android' ? 'http://10.0.2.2:8000/api' : 'http://localhost:8000/api';
 
 const API_PRODUCTION_URL = 'https://order-software.com/app-builder-backend/api';
 
