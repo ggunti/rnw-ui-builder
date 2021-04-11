13d33e1f4176eacd6eb79007e3f6aa320d417f4b
Guntter <ggunty96@gmail.com>
2021-04-11 14:20:37 +0300

    screen header update for android & iOS apps

--- client/src/Router/Router.native.tsx
+++ client/src/Router/Router.native.tsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { View, Alert, StyleSheet } from 'react-native';
+import { Text, Icon, Button } from 'react-native-elements';
 import SplashScreen from 'react-native-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { connect, ConnectedProps } from 'react-redux';
+import {
+  resetUser,
+  // auth
+  logout,
+} from '../redux/actions';
 import LoginPage from '../screens/Login/Login.page';
 import ProjectsPage from '../screens/Projects/Projects.page';
 import ProjectPreviewPage from '../screens/ProjectPreview/ProjectPreview.page';
@@ -21,10 +28,30 @@ class Router extends Component<RouterProps> {
     SplashScreen.hide();
   }
 
+  onPressLogout = () => {
+    const onSuccess = () => this.props.resetUser();
+    const onError = () => Alert.alert('Something went wrong.');
+    this.props.logout(onSuccess, onError);
+  };
+
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerBackTitleVisible: false,
+            headerTitle: (titleProps) => (
+              <View style={styles.row}>
+                <Icon name='build' type='material' containerStyle={{ marginRight: 5 }} />
+                <Text>{titleProps.children}</Text>
+              </View>
+            ),
+            headerRight: () =>
+              this.props.signedIn && (
+                <Button titleStyle={styles.logoutTitle} type='clear' title='Log out' onPress={this.onPressLogout} />
+              ),
+          }}
+        >
           {this.props.signedIn ? (
             <>
               <Stack.Screen name='projects' options={{ title: 'Projects' }} component={ProjectsPage} />
@@ -44,11 +71,25 @@ class Router extends Component<RouterProps> {
   }
 }
 
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  logoutTitle: {
+    fontSize: 16,
+  },
+});
+
 const mapStateToProps = (state: RootState) => {
   const { signedIn } = state.user;
   return { signedIn };
 };
 
-const connector = connect(mapStateToProps, {});
+const connector = connect(mapStateToProps, {
+  resetUser,
+  // auth
+  logout,
+});
 
 export default connector(Router);
