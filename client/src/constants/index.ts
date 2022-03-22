57ebd00e086370696669764120b71b9c9a3b0170
Guntter <ggunty96@gmail.com>
2022-03-22 18:03:56 +0200

    modified API_PRODUCTION_URL and .env.example file

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -4,7 +4,7 @@ const __DEV__ = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 const API_DEV_URL = Platform.OS === 'android' ? 'http://10.0.2.2:8000/api' : 'http://localhost:8000/api';
 
-const API_PRODUCTION_URL = 'https://order-software.com/app-builder-backend/api';
+const API_PRODUCTION_URL = 'https://order-software.com/ui-builder-backend/api';
 
 export const API_BASE_URL = __DEV__ ? API_DEV_URL : API_PRODUCTION_URL;
 
